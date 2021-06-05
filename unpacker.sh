b8903c841e4ea380071e743b306de43958b08474
Ppamo <pablo@ppamo.cl>
2021-06-05 18:10:14 -0400

    Improve auth token calculus

--- unpacker.sh
+++ unpacker.sh
@@ -1,12 +1,56 @@
 #!/bin/bash
 
 IMAGE=alpine:3.15.0
-# IMAGE=ubuntu:latest
+IMAGE=ubuntu:latest
 IMAGE=ppamo/mq-access-pre:v0.1.5
 REPO=${IMAGE%:*}
+echo "$REPO" | grep "/" > /dev/null
+if [ $? -ne 0 ]; then
+	REPO=library/$REPO
+fi
 VERSION=${IMAGE#*:}
 
-echo "+ Getting $REPO - $VERSION"
+TMPFILE=.output.dat
+AUTH_SERVER=https://auth.docker.io
+AUTH_SERVICE=registry.docker.io
+REGISTRY_SERVER=https://registry-1.docker.io
+
+TOKEN=
+
+getToken(){
+
+	printf "> Getting auth token:\n"
+	rm -f $TMPFILE
+	curl -fsSL "$AUTH_SERVER/token?service=$AUTH_SERVICE&scope=repository:$REPO:pull" -o $TMPFILE
+	TOKEN=$(grep -Eo '"token"[^:]*:[^"]*"[^"]*"' $TMPFILE | awk -F\" '{ print $4 }')
+	if [ ${#TOKEN} -gt 16 ]; then
+		printf -- "< Got token!\n$TOKEN\n"
+	else
+		printf "!ERROR: could not get the token\n"
+		exit 1
+	fi
+}
+
+getManifest(){
+	printf "> Getting manifest for $REPO:$VERSION:\n"
+	curl --request 'GET' \
+		--header "Authorization: Bearer ${TOKEN}" \
+		"$REGISTRY_SERVER/v2/$REPO/manifests/$VERSION" -o $TMPFILE
+	cat $TMPFILE | jq
+}
+
+echo "* Authenticating"
+getToken
+echo "* Getting data:"
+getManifest
+exit 0
+
+
+
+
+
+
+
 
 TOKEN="$(curl \
 	--silent \
@@ -19,9 +63,17 @@ echo "+ Got token:
 $TOKEN
 "
 
-LAYERS=$(curl --request 'GET' \
+echo "=> 1"
+curl --request 'GET' \
 	--header "Authorization: Bearer ${TOKEN}" \
-	"https://registry-1.docker.io/v2/library/$REPO/manifests/$VERSION" | jq -r '.fsLayers[].blobSum')
+	"https://registry-1.docker.io/v2/library/$REPO/manifests/$VERSION"
+exit 0
+
+# LAYERS=$(curl --request 'GET' \
+	# --header "Authorization: Bearer ${TOKEN}" \
+	# "https://registry-1.docker.io/v2/library/$REPO/manifests/$VERSION" | jq -r '.fsLayers[].blobSum')
+
+# "https://registry-1.docker.io/v2/library/$REPO/manifests/$VERSION" | jq -r '.fsLayers[].blobSum')
 
 
 echo "+ Got layers:
@@ -35,11 +87,11 @@ do
 	# FILENAME=layer.gz
 	rm -f $FILENAME
 	echo "+ Downloading $LAYER"
-	curl --request 'GET' \
-		--location \
-		--header "Authorization: Bearer ${TOKEN}" \
-		--output "$FILENAME" \
-		"https://registry-1.docker.io/v2/library/$REPO/blobs/$LAYER"
-	echo "+ Gunziping $FILENAME"
-	gunzip $FILENAME
+	# curl --request 'GET' \
+		# --location \
+		# --header "Authorization: Bearer ${TOKEN}" \
+		# --output "$FILENAME" \
+		# "https://registry-1.docker.io/v2/library/$REPO/blobs/$LAYER"
+	# echo "+ Gunziping $FILENAME"
+	# gunzip $FILENAME
 done
